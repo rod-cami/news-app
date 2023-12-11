449dde5f314fe7a8c0eb90b89b2ca71e8dffc64d
rod-cami <camilarodriguez4646@gmail.com>
2023-12-11 15:43:17 -0300

    Terminada funcionalidad del selector de tecnologías (Angular, Reactjs, Vuejs) y la vista para todas las noticias y para las favoritas

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Selector from '../../components/Selector/Selector'
 import NewsList from '../../components/NewsListComponent/NewsList'
 
-const Home = () => {
+const Home = ({setPage, setQuery, query,view}) => {
+
   return (
     <main className='content'>
-      <Selector/>
-      <NewsList/>
+      <Selector setQuery={setQuery}/>
+      <NewsList setQuery={setQuery} setPage={setPage} query={query} view={view}/>
     </main>
   )
 }
